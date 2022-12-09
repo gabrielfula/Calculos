2437182ac8cc2b50c7539ae443cf27531f08a979
Gabrielfula <gabriel.fulaneto0301@gmail.com>
2022-12-08 23:25:50 -0300

    ajustando responsividade

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-
+var sinal = document.getElementById('sinal');
 
 function somar() {
     let n1 = parseInt(document.getElementById("v1").value);
     let n2 = parseInt(document.getElementById("v2").value);
     document.getElementById("res").innerHTML = "Resultado Final: " + (n1 + n2);
+    sinal.innerText = " + "
 }
 
 
@@ -11,6 +12,14 @@ function divisao() {
     let n1 = parseInt(document.getElementById("v1").value)
     let n2 = parseInt(document.getElementById("v2").value)
     document.getElementById("res").innerHTML = "Resultado Final: " + (n1 / n2)
+    sinal.innerText = " / "
+}
+
+function subtrair() {
+    let n1 = parseInt(document.getElementById("v1").value)
+    let n2 = parseInt(document.getElementById("v2").value)
+    document.getElementById("res").innerHTML = "Resultado Final: " + (n1 - n2)
+    sinal.innerText = " - "
 }
 
 
@@ -18,9 +27,11 @@ function multiplicar() {
     let n1 = parseInt(document.getElementById("v1").value)
     let n2 = parseInt(document.getElementById("v2").value)
     document.getElementById("res").innerHTML = "Resultado Final: " + (n1 * n2)
+    sinal.innerText = " * "
 }
 
 function LimparForm() {
     document.getElementById("v1").value = "";
     document.getElementById("v2").value = "";
+    sinal.innerText = ""
 }
